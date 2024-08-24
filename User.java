3d84a90223cdf6b9b000ddabcd546e14fbc559fb
Mauryflow2004 <148460660+Mauryflow2004@users.noreply.github.com>
2024-08-23 22:22:29 -0500

    User.java

---
+++ User.java
@@ -0,0 +1,14 @@
+package com.example.songapp.model;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
