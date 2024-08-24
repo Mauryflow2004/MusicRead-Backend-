ecd4ab2f3e112c682e7f734f73516cd1e058edc0
Mauryflow2004 <148460660+Mauryflow2004@users.noreply.github.com>
2024-08-23 22:41:32 -0500

    UserService.java

---
+++ UserService.java
@@ -0,0 +1,20 @@
+package com.example.songapp.service;
+
+import com.example.songapp.model.User;
+import com.example.songapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
