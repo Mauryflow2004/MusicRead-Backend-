682f99a87ad37dda9e0a2e27805ab319ca98e948
Mauryflow2004 <148460660+Mauryflow2004@users.noreply.github.com>
2024-08-23 22:45:02 -0500

    SongAppApplication.java

---
+++ SongAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.songapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SongAppApplication.class, args);
+    }
+}
